a1a39df3c9ed5d85de095f52a796393c3969a7ef
z3r0bot <z3r0bot@gmail.com>
2025-04-24 16:44:48 -0700

    asd2

--- lib/shopify.ts
+++ lib/shopify.ts
@@ -4,6 +4,7 @@ import Client from 'shopify-buy';
 const client = Client.buildClient({
   domain: process.env.NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN || '',
   storefrontAccessToken: process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN || '',
+  apiVersion: '2023-10',
 });
 
 export default client; 
